a28ff2cd74f7c9cf1ee1d5c4e5e32c633cdd64d6
mobilisespiders <mobilisespiders@gmail.com>
2026-04-14 15:57:16 +0530

    Improve database connection setup and error handling
    Refactor database connection logic for better error handling and configuration.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,29 +1,33 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-print(os)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = None
+SessionLocal = None
 
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
-
-
-print(SessionLocal)
+if DATABASE_URL and DATABASE_URL.startswith("mysql"):
+    try:
+        engine = create_engine(DATABASE_URL)
+        SessionLocal = sessionmaker(bind=engine)
+        print("✅ Database connected")
+    except Exception as e:
+        print("❌ DB connection failed:", e)
+else:
+    print("⚠️ Skipping DB connection (DB not ready)")
 
 
 # This function creates a new database session for each request
 def get_db():
-    db = SessionLocal()   # create connection
+    if SessionLocal is None:
+        raise Exception("Database not configured yet")
+
+    db = SessionLocal()
     try:
-        yield db          # give connection to API
+        yield db
     finally:
-        db.close()        # always close connection (important!)
+        db.close()
 
 
 
